50427b3c5a4e8d4aa74ba9ffd0602f6cf261850a
Tiago Pala Baraúna <tiago_pala@outlook.com>
2020-07-09 04:32:44 -0300

    Criação de Tag Helpers personalizados, e configuração da aplicação para trabalhar com Áreas compartilhando a mesma página de Layout

--- src/MvcCore.Site/Startup.cs
+++ src/MvcCore.Site/Startup.cs
@@ -32,6 +32,8 @@ namespace MvcCore.Site
             app.UseMvc(routes =>
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
