72001d39d1e7388122d44fa93ae99557f546f234
Tiago Pala Baraúna <tiago_pala@outlook.com>
2020-07-09 02:34:50 -0300

    Adicionando bibliotecas externas por meio de CDN, aplicando conceitos de bundling e minification, deixando a aplicação de produção mais leve

--- src/MvcCore.Site/Startup.cs
+++ src/MvcCore.Site/Startup.cs
@@ -27,6 +27,8 @@ namespace MvcCore.Site
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
