dca09771d2b597a20cc55c3de940df8327916254
Tiago Pala Baraúna <tiago_pala@outlook.com>
2020-07-19 05:43:47 -0300

    Instalação do EntityFrameworkCore e configuração do DbContext.

---
+++ src/MvcCore.Site/Data/CustomDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCore.Site.Data
+{
+    public class CustomDbContext : DbContext
+    {
+        public CustomDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
--- src/MvcCore.Site/Startup.cs
+++ src/MvcCore.Site/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MvcCore.Site.Data;
 
@@ -14,6 +16,13 @@ namespace MvcCore.Site
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,8 +35,10 @@ namespace MvcCore.Site
                 options.AreaViewLocationFormats.Add("/Modulos/Shared/Views/{0}.cshtml");
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<CustomDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("CustomDbContext")));
 
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             #region DIP
             services.AddTransient<IPedidoRepository, PedidoRepository>();
